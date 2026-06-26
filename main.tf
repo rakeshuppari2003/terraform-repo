e28e6bbebfba305b52c854ab14b4bed0378c363a
rakeshuppari2003 <upparirakesh2003@gmail.com>
2026-06-26 13:08:01 +0530

    Update AMI ID for AWS instance

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ bucket = "my-githubactions-demo-bucket"
 }
 
 resource "aws_instance" "my_server" {
-  ami           = "ami-08f44e8eca9095668" 
+  ami           = "ami-0b6d9d3d33ba97d99" 
   instance_type = "t3.micro"             
 
   tags = {
